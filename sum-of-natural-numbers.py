9447c478d48d235122bbb10503427061f3576c7a
NandiniKarnakoti <132219274+NandiniKarnakoti@users.noreply.github.com>
2025-08-02 23:55:28 +0530

    Update sum-of-natural-numbers.py

--- sum-of-natural-numbers.py
+++ sum-of-natural-numbers.py
@@ -1,2 +1,17 @@
+#Naive Approach
+n=int(input()
+sum_val=0
+for i in range(n):
+  sum_val+=i
+print(sum_val)
+#Efficient Solution
+n=int(input()
+val=0
+for i in range(1,n):
+  for j in range(1,i):
+    val+=1
+print(val)
+#Optimized Approach
 n=int(input("Enter a number:"))
 print(n*(n+1)//2)
+
