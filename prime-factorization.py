b5c1b6abe8654175c1d33e059e1314bc479ae0a8
NandiniKarnakoti <132219274+NandiniKarnakoti@users.noreply.github.com>
2025-08-06 23:48:49 +0530

    Create prime-factorization.py

---
+++ prime-factorization.py
@@ -0,0 +1,16 @@
+#Naive Approach
+def isPrime(x):
+  for i in range(2,x):
+    if x%i==0:
+      return False
+  return True
+def primeFactors(n):
+  for i in range(2,n+1):
+    if isPrime(i):
+      x=i
+      while n%i==0:
+        print(i)
+      x=x*i
+if __name__=='__main__':
+  n=int(input())
+  print(primeFactors(n))
